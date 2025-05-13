070e9182105287780ffca13f8847c9421375f1da
PHELLIPE CUNICO DA ROSA <phellipe.cunico@cs.up.edu.br>
2025-05-13 11:26:50 -0300

    Criando o metodo GET e GET por ID / adicionando dados ao DB

--- Biblioteca-API/Program.cs
+++ Biblioteca-API/Program.cs
@@ -9,7 +9,20 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
-// Rota de teste
-app.MapGet("/livros", () => "Biblioteca funcionando corretamente!");
+// Metódo GET - Listar os livros
+app.MapGet("/livros", (AppDbContext db) =>
+{
+    var livros = db.TabelaLivros.ToList();
+    return Results.Ok(livros);
+});
 
+// Metódo GET por ID - Listar os livros por ID
+app.MapGet("/livros/{id}", async (int id, AppDbContext db) =>
+{
+    var livros = await db.TabelaLivros.FindAsync(id);
+    return livros is not null ? Results.Ok(livros) : Results.NotFound("Livro não encontrado, tente novamente!");
+});
 app.Run();
+
+
+
